20da8c2dff5c121ad4a05f5958594524902dea6d
KeiberLLB <leandro1991.9@gmail.com>
2024-07-24 12:02:53 -0500

    save settings

--- SpringBoot/_06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/utils/messages/ErrorMessages.java
+++ SpringBoot/_06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/utils/messages/ErrorMessages.java
@@ -11,10 +11,10 @@ import lombok.experimental.SuperBuilder;
 public class ErrorMessages {
 
     private final String mensaje = "hola";
-    
-    public static String idNotFound(String entity){
-    //    return "No hay registros en la entidad "+entity+ " con el id suministrado";
-       final String message = "No hay registros en la entidad %s con el id suministrado";
-       return String.format(message, entity);
+
+    public static String idNotFound(String entity) {
+        // return "No hay registros en la entidad "+entity+ " con el id suministrado";
+        final String message = "No hay registros en la entidad %s con el id suministrado";
+        return String.format(message, entity);
     }
 }
